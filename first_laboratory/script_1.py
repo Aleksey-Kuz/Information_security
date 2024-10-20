3c994459016436bdc4081a25775221eab595c8b2
Aleksey Kuznetsov <alex89202226090@yandex.ru>
2024-10-20 11:36:58 +0300

    Implemented make_matrix() function

--- first_laboratory/script_1.py
+++ first_laboratory/script_1.py
@@ -1,7 +1,34 @@
-# This file contains a script demonstrating the encryption of a string from
-# input.txt in encrypted.txt and decryption in decrypted.txt
+"""
+This file contains a script demonstrating the encryption of a string from
+input.txt in encrypted.txt and decryption in decrypted.txt
+"""
 
 
+# dependencies
+import json
 
 
+def make_matrix(text_str: str, n: int = 5) -> list[list]:
+    """
+        This function converts a string into a matrix of the specified size
+    :param text_str: the input line
+    :param n: the size of the matrix
+    :return: the matrix for encoding
+    """
+    text_str = text_str.strip()
+    text_str += ' ' * (n - len(text_str) % n)
 
+    return [list(text_str[line*5:line*5+5]) for line in range(len(text_str)//n)]
+
+
+# main script
+with open("input.txt", 'r', encoding='utf-8') as input_file:
+    text = input_file.read()
+with open("config.json", 'r') as config_file:
+    params = json.load(config_file)
+
+print(list(text))
+r = make_matrix(text)
+for l in r:
+    print(l)
+print(params)
