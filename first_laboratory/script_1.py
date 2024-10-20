966d9297983b7d7aba3a31aa1e504c15fbf69a7e
Aleksey Kuznetsov <alex89202226090@yandex.ru>
2024-10-20 12:00:29 +0300

    Implemented encoding() function

--- first_laboratory/script_1.py
+++ first_laboratory/script_1.py
@@ -21,6 +21,35 @@ def make_matrix(text_str: str, n: int = 5) -> list[list]:
     return [list(text_str[line*5:line*5+5]) for line in range(len(text_str)//n)]
 
 
+def encoding(matrix: list[list], k1: list, k2: list) -> list[list]:
+    """
+        This function encodes the text according to the matrix
+    :param matrix: the input origin matrix
+    :param k1: params for columns
+    :param k2: params for lines
+    :return: the encoded matrix
+    """
+    encoding_matrix = list()
+
+    for line in range(len(k2)):
+        encoding_matrix.append(list())
+        for column in range(len(k1)):
+            encoding_matrix[line].append(matrix[k2[line]-1][k1[column]-1])
+
+    return encoding_matrix
+
+
+def decoding(matrix: list[list], k1: list, k2: list) -> list[list]:
+    """
+        This function decoding the text according to the matrix
+    :param matrix: the input origin matrix
+    :param k1: params for columns
+    :param k2: params for lines
+    :return: the decoded matrix
+    """
+    pass
+
+
 # main script
 with open("input.txt", 'r', encoding='utf-8') as input_file:
     text = input_file.read()
@@ -32,3 +61,13 @@ r = make_matrix(text)
 for l in r:
     print(l)
 print(params)
+
+print('input line')
+print('matrix')
+print('matrix encoding')
+print('encoding')
+print('matrix decoding')
+print('decoding')
+
+for l in encoding(r, params['k1'], params['k2']):
+    print(l)
