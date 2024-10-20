a4a69fa32a96a07a6e4c44cefc978c4aed751dad
Aleksey Kuznetsov <alex89202226090@yandex.ru>
2024-10-20 12:25:32 +0300

    Implemented make_text() function

--- first_laboratory/script_1.py
+++ first_laboratory/script_1.py
@@ -13,12 +13,23 @@ def make_matrix(text_str: str, n: int = 5) -> list[list]:
         This function converts a string into a matrix of the specified size
     :param text_str: the input line
     :param n: the size of the matrix
-    :return: the matrix for encoding
+    :return: the matrix for encoding/decoding
     """
     text_str = text_str.strip()
     text_str += ' ' * (n - len(text_str) % n)
 
     return [list(text_str[line*5:line*5+5]) for line in range(len(text_str)//n)]
+
+
+def make_text(matrix: list[list]) -> str:
+    """
+        This function converts the encoded/decoded matrix into a string
+    :param matrix: the encoded/decoded matrix
+    :return: the line from encoded/decoded matrix
+    """
+    return ''.join([''.join([matrix[ind_c][ind_l]
+                             for ind_c in range(len(matrix))])
+                    for ind_l in range(len(matrix))])
 
 
 def encoding(matrix: list[list], k1: list, k2: list) -> list[list]:
@@ -47,7 +58,14 @@ def decoding(matrix: list[list], k1: list, k2: list) -> list[list]:
     :param k2: params for lines
     :return: the decoded matrix
     """
-    pass
+    decoding_matrix = list()
+
+    for line in range(len(k2)):
+        decoding_matrix.append(list())
+        for column in range(len(k1)):
+            decoding_matrix[line].append(matrix[k2[line]-1][k1[column]-1])
+
+    return decoding_matrix
 
 
 # main script
@@ -56,18 +74,25 @@ with open("input.txt", 'r', encoding='utf-8') as input_file:
 with open("config.json", 'r') as config_file:
     params = json.load(config_file)
 
-print(list(text))
-r = make_matrix(text)
-for l in r:
-    print(l)
-print(params)
+print('---Input text---')
+print(text)
+print('\n-------------------------\n')
 
-print('input line')
-print('matrix')
-print('matrix encoding')
-print('encoding')
+origin_matrix = make_matrix(text)
+print('---Origin matrix---')
+for line in origin_matrix:
+    print(line)
+print('\n-------------------------\n')
+
+enc_matrix = encoding(origin_matrix, params['k1'], params['k2'])
+enc_text = make_text(enc_matrix)
+print('---Encoding text---')
+print(enc_text)
+print('---Matrix encoding---')
+print(enc_matrix)
+print('\n-------------------------\n')
+
 print('matrix decoding')
 print('decoding')
-
-for l in encoding(r, params['k1'], params['k2']):
-    print(l)
+m = encoding(make_matrix(enc_text), params['k1'], params['k2'])
+print(m)
